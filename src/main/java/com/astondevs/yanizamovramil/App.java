8b0434a545870fb94826240dd7a5b87d2f2f24dc
Ramil Nizamov <yanizamovramil@yandex.ru>
2023-03-07 22:29:23 +0300

    init main

---
+++ src/main/java/com/astondevs/yanizamovramil/App.java
@@ -0,0 +1,10 @@
+package com.astondevs.yanizamovramil;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
